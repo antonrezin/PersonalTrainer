16e06117435de89a6d4d3555cd3e8e29508bf030
Anton Rezin <rezin.anton@gmail.com>
2024-11-23 18:29:18 +0200

    Added a Customers section to the Ag-Grid component and displayed data from the Customers API.

--- src/components/Customers/CustomersList.jsx
+++ src/components/Customers/CustomersList.jsx
@@ -1,11 +1,63 @@
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-material.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 import { useEffect, useState } from "react";
 
 function CustomersList() {
   // Saving fetched data to useState
-  const [customersList, setCustomersList] = useState("");
+  const [customers, setCustomers] = useState([]);
+
+  // Ag-grid column default properties function
+  const columnProps = () => ({
+    sortable: true,
+    filter: true,
+    suppressSizeToFit: true,
+  });
+
+  // Defining Ag-grid colums
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      headerName: "ID",
+      valueGetter: "node.rowIndex + 1",
+      width: 70,
+      ...columnProps(),
+    },
+    {
+      headerName: "First Name",
+      field: "firstname",
+      ...columnProps(),
+    },
+    {
+      headerName: "Last Name",
+      field: "lastname",
+      ...columnProps(),
+    },
+    {
+      headerName: "Street Address",
+      field: "streetaddress",
+      ...columnProps(),
+    },
+    {
+      headerName: "Postcode",
+      field: "postcode",
+      ...columnProps(),
+    },
+    {
+      headerName: "City",
+      field: "city",
+      ...columnProps(),
+    },
+    {
+      headerName: "Email",
+      field: "email",
+      ...columnProps(),
+    },
+    {
+      headerName: "Phone",
+      field: "phone",
+      ...columnProps(),
+    },
+  ]);
 
   // Fetch customers data
   const fetchData = () => {
@@ -19,7 +71,7 @@ function CustomersList() {
         return response.json();
       })
       .then((data) => {
-        setCustomersList(data._embedded.customers);
+        setCustomers(data._embedded.customers);
         console.log(data._embedded.customers);
       })
       .catch((err) => console.log(err));
@@ -29,7 +81,22 @@ function CustomersList() {
     fetchData();
   }, []);
 
-  return <div></div>;
+  return (
+    <div
+      className="ag-theme-alpine-dark"
+      style={{
+        height: "130vh",
+        minWidth: "350px",
+        maxWidth: "1470px",
+        margin: "auto",
+        alignItems: "center",
+        fontSize: "17px",
+        textAlign: "center",
+      }}
+    >
+      <AgGridReact rowData={customers} columnDefs={columnDefs}></AgGridReact>
+    </div>
+  );
 }
 
 export default CustomersList;
