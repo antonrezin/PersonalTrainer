127741c7cd8238c752c71aedbcfa7b5dab631c33
Anton Rezin <rezin.anton@gmail.com>
2024-11-24 18:51:19 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import MyAppBar from "./components/AppBar/MyAppBar";
 import CustomersList from "./components/Customers/CustomersList";
+import TrainingsList from "./components/Trainings/TrainingsList";
 
 function App() {
   return (
